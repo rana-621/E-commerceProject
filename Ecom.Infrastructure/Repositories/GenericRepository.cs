2715233a1c2488d2da2da0973afc240f104d1928
rana-621 <ranaebrahim621@gmail.com>
2025-10-18 19:34:28 +0300

    Implement the GenericRepo

---
+++ Ecom.Infrastructure/Repositories/GenericRepository.cs
@@ -0,0 +1,42 @@
+﻿using Ecom.Core.Interfaces;
+using System.Linq.Expressions;
+
+namespace Ecom.Infrastructure.Repositories;
+
+public class GenericRepository<T> : IGenericRepository<T> where T : class
+{
+    Task<T> IGenericRepository<T>.AddAsync(T entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    Task IGenericRepository<T>.DeleteAsync(T entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    Task<IReadOnlyList<T>> IGenericRepository<T>.GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    Task<IReadOnlyList<T>> IGenericRepository<T>.GetAllAsync(params Expression<Func<T, object>>[] includes)
+    {
+        throw new NotImplementedException();
+    }
+
+    Task<T> IGenericRepository<T>.GetByIdAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    Task<T> IGenericRepository<T>.GetByIdAsync(int id, params Expression<Func<T, object>>[] includes)
+    {
+        throw new NotImplementedException();
+    }
+
+    Task IGenericRepository<T>.UpdateAsync(T entity)
+    {
+        throw new NotImplementedException();
+    }
+}
