6ef2be7aab3b57defc6a39d9cd4b0b8f00c92b7e
rana-621 <ranaebrahim621@gmail.com>
2025-10-18 22:25:41 +0300

    Add DbContext and install packages

---
+++ Ecom.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Ecom.Core.Entities.Product;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ecom.Infrastructure.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions options) : base(options)
+    {
+
+    }
+    public virtual DbSet<Product> Products { get; set; }
+    public virtual DbSet<Category> Categories { get; set; }
+    public virtual DbSet<Photo> Photos { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
