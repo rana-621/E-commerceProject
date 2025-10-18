ae5d4bbea2d00c99fca1cad7e5616416a5735901
rana-621 <ranaebrahim621@gmail.com>
2025-10-18 19:30:21 +0300

    Create IGenericRepo

---
+++ Ecom.Core/Interfaces/IGenericRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq.Expressions;
+
+namespace Ecom.Core.Interfaces;
+
+public interface IGenericRepository<T> where T : class
+{
+    Task<T> GetByIdAsync(int id);
+    Task<T> GetByIdAsync(int id, params Expression<Func<T, object>>[] includes);
+    Task<IReadOnlyList<T>> GetAllAsync();
+    Task<IReadOnlyList<T>> GetAllAsync(params Expression<Func<T, object>>[] includes);
+    Task<T> AddAsync(T entity);
+    Task UpdateAsync(T entity);
+    Task DeleteAsync(T entity);
+}
