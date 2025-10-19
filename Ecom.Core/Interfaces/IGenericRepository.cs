744069330cb38e8ed88e1ee7d8fbf4afec498a69
rana-621 <ranaebrahim621@gmail.com>
2025-10-19 22:08:46 +0300

    Complete Generic methods

--- Ecom.Core/Interfaces/IGenericRepository.cs
+++ Ecom.Core/Interfaces/IGenericRepository.cs
@@ -8,7 +8,8 @@ public interface IGenericRepository<T> where T : class
     Task<T> GetByIdAsync(int id, params Expression<Func<T, object>>[] includes);
     Task<IReadOnlyList<T>> GetAllAsync();
     Task<IReadOnlyList<T>> GetAllAsync(params Expression<Func<T, object>>[] includes);
-    Task<T> AddAsync(T entity);
+    Task AddAsync(T entity);
     Task UpdateAsync(T entity);
-    Task DeleteAsync(T entity);
+    Task DeleteAsync(int id);
+
 }
--- Ecom.Infrastructure/Repositories/GenericRepository.cs
+++ Ecom.Infrastructure/Repositories/GenericRepository.cs
@@ -1,41 +1,50 @@
 ﻿using Ecom.Core.Interfaces;
+using Ecom.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
 using System.Linq.Expressions;
 
 namespace Ecom.Infrastructure.Repositories;
 
 public class GenericRepository<T> : IGenericRepository<T> where T : class
 {
-    Task<T> IGenericRepository<T>.AddAsync(T entity)
+    private readonly AppDbContext _context;
+    public GenericRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+    public async Task AddAsync(T entity)
+    {
+        await _context.Set<T>().AddAsync(entity);
+        await _context.SaveChangesAsync();
+
+    }
+
+    public async Task DeleteAsync(int id)
+    {
+        var entity = await _context.Set<T>().FindAsync(id);
+        _context.Set<T>().Remove(entity);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task<IReadOnlyList<T>> GetAllAsync()
+        => await _context.Set<T>().AsNoTracking().ToListAsync();
+
+    public Task<IReadOnlyList<T>> GetAllAsync(params Expression<Func<T, object>>[] includes)
     {
         throw new NotImplementedException();
     }
 
-    Task IGenericRepository<T>.DeleteAsync(T entity)
+    public Task<T> GetByIdAsync(int id)
     {
         throw new NotImplementedException();
     }
 
-    Task<IReadOnlyList<T>> IGenericRepository<T>.GetAllAsync()
+    public Task<T> GetByIdAsync(int id, params Expression<Func<T, object>>[] includes)
     {
         throw new NotImplementedException();
     }
 
-    Task<IReadOnlyList<T>> IGenericRepository<T>.GetAllAsync(params Expression<Func<T, object>>[] includes)
-    {
-        throw new NotImplementedException();
-    }
-
-    Task<T> IGenericRepository<T>.GetByIdAsync(int id)
-    {
-        throw new NotImplementedException();
-    }
-
-    Task<T> IGenericRepository<T>.GetByIdAsync(int id, params Expression<Func<T, object>>[] includes)
-    {
-        throw new NotImplementedException();
-    }
-
-    Task IGenericRepository<T>.UpdateAsync(T entity)
+    public Task UpdateAsync(T entity)
     {
         throw new NotImplementedException();
     }
