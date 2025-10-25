f9c9ba0e5d76c01fe6228315d5f4f51056690a49
rana-621 <ranaebrahim621@gmail.com>
2025-10-25 23:35:29 +0300

    Add infrastructure Registeration

---
+++ Ecom.Core/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,8 @@
+﻿using Ecom.Core.Entities.Product;
+
+namespace Ecom.Core.Interfaces;
+
+public interface ICategoryRepository : IGenericRepository<Category>
+{
+    //for future category specific methods
+}
--- Ecom.Infrastructure/infrastructureRegisteration.cs
+++ Ecom.Infrastructure/infrastructureRegisteration.cs
@@ -1,5 +1,18 @@
-﻿namespace Ecom.Infrastructure;
+﻿using Ecom.Core.Interfaces;
+using Ecom.Infrastructure.Repositories;
+using Microsoft.Extensions.DependencyInjection;
 
-public class infrastructureRegisteration
+namespace Ecom.Infrastructure;
+
+public static class infrastructureRegisteration
 {
+    public static IServiceCollection infrastructureConfiguration(this IServiceCollection services)
+    {
+        // services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+        //services.AddTransient
+        //services.AddScoped
+        //services.AddSingleton
+        services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+        return services;
+    }
 }
