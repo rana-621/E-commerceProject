5c7e4fe37278ae07258f019175c76ba912d2db89
rana-621 <ranaebrahim621@gmail.com>
2025-10-25 23:37:28 +0300

    Implement CategoryRepository

---
+++ Ecom.Infrastructure/Repositories/CategoryRepository.cs
@@ -0,0 +1,12 @@
+﻿using Ecom.Core.Entities.Product;
+using Ecom.Core.Interfaces;
+using Ecom.Infrastructure.Data;
+
+namespace Ecom.Infrastructure.Repositories;
+
+public class CategoryRepository : GenericRepository<Category>, ICategoryRepository
+{
+    public CategoryRepository(AppDbContext context) : base(context)
+    {
+    }
+}
