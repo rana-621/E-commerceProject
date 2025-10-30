05c0def6aa5009ee8f9c50d5a3531aab00cc78a9
rana-621 <ranaebrahim621@gmail.com>
2025-10-30 21:10:35 +0300

    Add options dbContext to infraService Registeration

--- Ecom.Infrastructure/infrastructureRegisteration.cs
+++ Ecom.Infrastructure/infrastructureRegisteration.cs
@@ -1,16 +1,22 @@
 ﻿using Ecom.Core.Interfaces;
+using Ecom.Infrastructure.Data;
 using Ecom.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Ecom.Infrastructure;
 
 public static class infrastructureRegisteration
 {
-    public static IServiceCollection infrastructureConfiguration(this IServiceCollection services)
+    public static IServiceCollection infrastructureConfiguration(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
         services.AddScoped<IUnitOfWork, UnitOfWork>();
-
+        services.AddDbContext<AppDbContext>(op =>
+        {
+            op.UseSqlServer(configuration.GetConnectionString("EcomDatabase"));
+        });
         return services;
     }
 }
