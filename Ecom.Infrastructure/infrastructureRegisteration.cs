4ed7d465c90bd5804a440e90bfd9e9ca495349c9
rana-621 <ranaebrahim621@gmail.com>
2025-10-30 21:07:46 +0300

    Add ConnectionString To appSettings Api

--- Ecom.Infrastructure/infrastructureRegisteration.cs
+++ Ecom.Infrastructure/infrastructureRegisteration.cs
@@ -8,17 +8,7 @@ public static class infrastructureRegisteration
 {
     public static IServiceCollection infrastructureConfiguration(this IServiceCollection services)
     {
-        // services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
-        //services.AddTransient
-        //services.AddScoped
-        //services.AddSingleton
-
-
         services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
-
-        //services.AddScoped<ICategoryRepository, CategoryRepository>();
-        //services.AddScoped<IProductRepository, ProductRepository>();    
-        //services.AddScoped<IPhotoRepository, PhotoRepository>();
         services.AddScoped<IUnitOfWork, UnitOfWork>();
 
         return services;
