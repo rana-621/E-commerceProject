b8d01c5a8571dbb444adfd963595db8a7beab9c6
rana-621 <ranaebrahim621@gmail.com>
2025-10-30 19:30:17 +0300

    Add Class Photo

---
+++ Ecom.Core/Interfaces/IPhotoRepository.cs
@@ -0,0 +1,8 @@
+﻿using Ecom.Core.Entities.Product;
+
+namespace Ecom.Core.Interfaces
+{
+    public interface IPhotoRepository : IGenericRepository<Photo>
+    {
+    }
+}
---
+++ Ecom.Infrastructure/Repositories/PhotoRepository.cs
@@ -0,0 +1,13 @@
+﻿using Ecom.Core.Entities.Product;
+using Ecom.Core.Interfaces;
+using Ecom.Infrastructure.Data;
+
+namespace Ecom.Infrastructure.Repositories
+{
+    public class PhotoRepository : GenericRepository<Photo>, IPhotoRepository
+    {
+        public PhotoRepository(AppDbContext context) : base(context)
+        {
+        }
+    }
+}
