12f5c1e599058dd16f23566205f409228bea66e1
rana-621 <ranaebrahim621@gmail.com>
2025-11-02 13:46:07 +0200

    Base and Category controller

---
+++ Ecom.API/Controllers/BaseController.cs
@@ -0,0 +1,17 @@
+﻿using Ecom.Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecom.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BaseController : ControllerBase
+    {
+        protected readonly IUnitOfWork unitOfWork;
+
+        public BaseController(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+    }
+}
---
+++ Ecom.API/Controllers/CategoriesController.cs
@@ -0,0 +1,29 @@
+﻿using Ecom.Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecom.API.Controllers
+{
+
+    public class CategoriesController : BaseController
+    {
+        public CategoriesController(IUnitOfWork unitOfWork) : base(unitOfWork)
+        {
+        }
+
+        [HttpGet("get-all")]
+        public async Task<IActionResult> GetAllCategories()
+        {
+            try
+            {
+                var categories = await unitOfWork.CategoryRepository.GetAllAsync();
+                if (categories is null)
+                    return BadRequest();
+                return Ok(categories);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
