8e388ae78738f54bd38957ce01fac3ca2da6fe78
rana-621 <ranaebrahim621@gmail.com>
2025-11-29 18:12:15 +0200

    Seeding photo to database

---
+++ Ecom.Infrastructure/Data/Config/PhotoConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Ecom.Core.Entities.Product;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Ecom.Infrastructure.Data.Config;
+
+public class PhotoConfiguration : IEntityTypeConfiguration<Photo>
+{
+    public void Configure(EntityTypeBuilder<Photo> builder)
+    {
+        builder.HasData(new Photo { Id = 3, ImageName = "test3", ProductId = 1 },
+                        new Photo { Id = 4, ImageName = "test4", ProductId = 1 });
+    }
+}
---
+++ Ecom.Infrastructure/Data/Migrations/20251129161104_SeedPhoto.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Ecom.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedPhoto : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Photos",
+                columns: new[] { "Id", "ImageName", "ProductId" },
+                values: new object[,]
+                {
+                    { 3, "test3", 1 },
+                    { 4, "test4", 1 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Photos",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Photos",
+                keyColumn: "Id",
+                keyValue: 4);
+        }
+    }
+}
