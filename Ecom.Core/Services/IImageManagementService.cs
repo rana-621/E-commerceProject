5ae5c5ed96ab16543fa63b0e9b4e1dc8ad047982
rana-621 <ranaebrahim621@gmail.com>
2025-11-30 23:59:29 +0200

    create service

---
+++ Ecom.Core/Services/IImageManagementService.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Ecom.Core.Services;
+
+public interface IImageManagementService
+{
+    Task<List<string>> AddImageAsync(IFormFileCollection files, string src)
+}
