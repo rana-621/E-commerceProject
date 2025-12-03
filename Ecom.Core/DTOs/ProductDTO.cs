fa2a65bae2acb0f2c0b27fdc38c5957843a3f798
rana-621 <ranaebrahim621@gmail.com>
2025-12-04 00:38:08 +0200

    Add CategoryId to the dto

--- Ecom.Core/DTOs/ProductDTO.cs
+++ Ecom.Core/DTOs/ProductDTO.cs
@@ -23,5 +23,7 @@ public record AddProductDTO
     public string Description { get; set; } = string.Empty;
     public decimal NewPrice { get; set; }
     public decimal OldPrice { get; set; }
+    public int CategoryId { get; set; }
+
     public IFormFileCollection Photo { get; set; }
 }
