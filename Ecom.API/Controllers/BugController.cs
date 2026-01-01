4b28286f8671e62f96a49285e6bdd33b01b62c6e
rana-621 <ranaebrahim621@gmail.com>
2026-01-01 15:39:46 +0200

    27-Create handle error and use posman

---
+++ Ecom.API/Controllers/BugController.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using Ecom.Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecom.API.Controllers
+{
+    public class BugController : BaseController
+    {
+        public BugController(IUnitOfWork unitOfWork, IMapper mapper) : base(unitOfWork, mapper)
+        {
+        }
+
+        [HttpGet("not-found")]
+        public async Task<ActionResult> GetNotFound()
+        {
+            var category = await unitOfWork.CategoryRepository.GetByIdAsync(42);
+            if (category == null) return NotFound();
+            return Ok(category);
+        }
+
+        [HttpGet("server-error")]
+        public async Task<ActionResult> GetServerError()
+        {
+            var category = await unitOfWork.CategoryRepository.GetByIdAsync(42);
+            category.Name = "";
+            return Ok(category);
+        }
+
+        [HttpGet("bad-request/{Id}")]
+        public async Task<ActionResult> GetBadRequest(int id)
+        {
+            return Ok();
+        }
+        [HttpGet("bad-request/")]
+        public async Task<ActionResult> GetBadRequest()
+        {
+            return BadRequest();
+        }
+    }
+}
