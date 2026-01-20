64c2c462940bd3516d02293d55ce8030b0105044
rana-621 <ranaebrahim621@gmail.com>
2026-01-20 13:42:56 +0200

    29- Security Part one Create Exception Middleware

---
+++ Ecom.API/Helper/ApiExceptions.cs
@@ -0,0 +1,14 @@
+﻿namespace Ecom.API.Helper;
+
+public class ApiExceptions : ResponseAPI
+{
+    public ApiExceptions(int statusCode, string message = null, string details = null) : base(statusCode, message)
+    {
+
+        Details = details;
+    }
+    public string Details
+    {
+        get; set;
+    }
+}
---
+++ Ecom.API/Middleware/ExceptionsMiddleware.cs
@@ -0,0 +1,30 @@
+﻿using Ecom.API.Helper;
+using System.Net;
+using System.Text.Json;
+
+namespace Ecom.API.Middleware;
+
+public class ExceptionsMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public ExceptionsMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception ex)
+        {
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            context.Response.ContentType = "application/json";
+            var response = new ApiExceptions((int)HttpStatusCode.InternalServerError, ex.Message, ex.StackTrace);
+            var json = JsonSerializer.Serialize(response);
+            context.Response.WriteAsync(json);
+        }
+    }
+}
